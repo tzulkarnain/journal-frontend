5f0902447125814c1b5b2340dd636c77350c00bf
Grace <grace.scharf@gmail.com>
2017-11-14 18:58:32 -0500

    completes landing page and adds links to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import WriteEntry from "./components/WriteEntry"
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        Hi I'm Gonna Be A Big Journal App Someday :O
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path="/" component={LandingPage} />
+          <footer>Curious Rose™</footer>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import blueBeige from '../images/bluebeige.jpg';
+import { Link } from 'react-router-dom';
+import styles from '../temp-css/landingPage.css'
+
 
 class LandingPage extends Component {
+  
   render() {
     return (
-      <div></div>
-    );
+      <div className="landingPage">
+        <img className="landingImg" src={blueBeige}/>
+        <div className="landingTextWrapper">
+          <h1>Welcome to Noctjournal</h1>
+          <div>
+          <Link to="/createaccount"><button>Create Account</button></Link>
+          <Link to="/login"><button>Log In</button></Link>
+          </div>
+        </div>
+      </div>
+      
+    )
   }
 }
 
