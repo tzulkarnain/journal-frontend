75b917dc208437a7d84785924e8c58dedaac7d5a
Timothy Zulkarnain <TZ@Timothys-MacBook-Air.local>
2017-11-14 17:45:55 -0500

    create account page

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class CreateAccount extends Component {
+  
   render() {
     return (
-      <div></div>
+      <div className="createAccount">
+        <h1>Create Account</h1>
+        <form onSubmit="this.handleSubmit">
+          <div className="inputFields">
+          <input type='text' placeholder="First Name" />
+          <input type='text' placeholder="Last Name" />
+          <input type='text' placeholder="Email" />
+          <input type='text' placeholder="Confirm Email" />
+          <input type='text' placeholder="Password" />
+          <input type='text' placeholder="Confirm Password" />
+          <button>Create Account</button>
+          </div>
+        </form>
+
+      </div>
     );
   }
 }
