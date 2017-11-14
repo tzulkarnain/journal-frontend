791c2b3a455fd41a9b962b0360b6eb9cec5dc483
Timothy Zulkarnain <TZ@Timothys-MacBook-Air.local>
2017-11-14 18:18:00 -0500

    create account page updated

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 
 class CreateAccount extends Component {
-  
+
   render() {
     return (
       <div className="createAccount">
         <h1>Create Account</h1>
-        <form onSubmit="this.handleSubmit">
+        <form onSubmit={this.handleSubmit}>
           <div className="inputFields">
-          <input type='text' placeholder="First Name" />
-          <input type='text' placeholder="Last Name" />
-          <input type='text' placeholder="Email" />
-          <input type='text' placeholder="Confirm Email" />
-          <input type='text' placeholder="Password" />
-          <input type='text' placeholder="Confirm Password" />
-          <button>Create Account</button>
+            <div>
+              <input type='text' placeholder="First Name" ref={r => this.highlightInp1 = r} />
+              <input type='text' placeholder="Last Name" ref={r => this.highlightInp2 = r} />
+            </div>
+            <div>
+              <input type='text' placeholder="Email" ref={r => this.highlightInp3 = r} />
+              <input type='text' placeholder="Confirm Email" ref={r => this.highlightInp4 = r} />
+            </div>
+            <div>
+              <input type='text' placeholder="Password" ref={r => this.highlightInp5 = r} />
+              <input type='text' placeholder="Confirm Password" ref={r => this.highlightInp6 = r} />
+            </div>
+            <button>Create Account</button>
           </div>
         </form>
 
