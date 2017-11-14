8fcc2c750b42ca15e7045960966e974f190c66ad
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2017-11-14 17:49:03 -0500

    add WriteEntry static component
    also adds PictureUpload and Slider components, but
    they're not functional yet.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import WriteEntry from "./components/WriteEntry"
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Hi I'm Gonna Be A Big Journal App Someday :O
       </div>
     );
   }
