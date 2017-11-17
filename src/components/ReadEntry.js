e1962748bf2ae667e65fd4a24f4ada0267fef7ff
Timothy Zulkarnain <TZ@Timothys-MacBook-Air.local>
2017-11-16 21:33:15 -0500

    read entry adjustments

--- src/components/ReadEntry.js
+++ src/components/ReadEntry.js
@@ -3,7 +3,7 @@ import NavBar from './NavBar'
 import api from '../api.js'
 import auth from '../auth.js';
 import { Grid, Segment, Header } from 'semantic-ui-react'
-import puppyPic from '../images/puppy.png';
+
 
 class ReadEntry extends Component {
   constructor() {
@@ -35,49 +35,43 @@ class ReadEntry extends Component {
     let newDateArray = timeStamp.split('T');
     let justDate = newDateArray[0];
     return justDate;
-    }
+  }
 
   render() {
     return (
       <div>
-        <NavBar hist={this.props.history}/>
+        <NavBar hist={this.props.history} />
         {!this.state.loaded ? <div>loading...</div> :
           <Grid columns="equal" padded>]
           <Grid.Column>
-            <Grid.Row>
-            <img alt="unsplash-or-chosen" src={this.state.singleEntry.full_image_url}/>
-            </Grid.Row>
+              <Grid.Row>
+                <img alt="unsplash-or-chosen" src={this.state.singleEntry.full_image_url} />
+              </Grid.Row>
             </Grid.Column>
             <Grid.Row>
               <Grid.Column>
-                  <Header as="h1">{this.state.singleEntry.title}</Header>
-                  <p>{this.displayDate(this.state.singleEntry.createdAt)}</p>
+                <Header as="h1">{this.state.singleEntry.title}</Header>
+                <p>{this.displayDate(this.state.singleEntry.createdAt)}</p>
               </Grid.Column>
             </Grid.Row>
-            <Grid.Row stretched>  
-          <Grid.Column>  
-                  
-          <Segment>
-              <h5>Rating: {this.state.singleEntry.mood}</h5>
-              <h5>Highlights: </h5>
-                <div>{this.state.singleEntry.q1a1}</div>
-                <div>{this.state.singleEntry.q1a3}</div>
-                <div>{this.state.singleEntry.q1a2}</div>
-            </Segment>
-          </Grid.Column>  
-          
-          
-          <Grid.Column>  
-           <Segment>
-              <h5>Could have done better:</h5>
-              <p>{this.state.singleEntry.q2}</p>
-              <h5>Always wanted to:</h5>
-              <p>{this.state.singleEntry.q3}</p>
-              <h5>Today's notes</h5>
-              <p>{this.state.singleEntry.q4}</p>
-           </Segment>
-          </Grid.Column>
-          </Grid.Row>
+            <Grid.Row stretched>
+              <Grid.Column>
+
+                <Segment>
+                  <h5>Rating: {this.state.singleEntry.mood}</h5>
+                  <h5>Highlights: </h5>
+                  <div>{this.state.singleEntry.q1a1}</div>
+                  <div>{this.state.singleEntry.q1a2}</div>
+                  <div>{this.state.singleEntry.q1a3}</div>
+                  <h5>Could have done better:</h5>
+                  <p>{this.state.singleEntry.q2}</p>
+                  <h5>Always wanted to:</h5>
+                  <p>{this.state.singleEntry.q3}</p>
+                  <h5>Today's notes</h5>
+                  <p>{this.state.singleEntry.q4}</p>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
           </Grid>
         }
       </div>
