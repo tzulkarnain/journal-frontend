665c19a866da57dc14cf25da37fdfd4d871513e1
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2017-11-20 12:17:25 -0500

    add console log for getEntries with a searchterm

--- src/api.js
+++ src/api.js
@@ -6,22 +6,22 @@ class Api {
 
     createAccount = (firstName, lastName, email, password) => {
         return superagent
-        .post(`${apiHost}/api/auth/create-account`)
-        .send({ firstName, lastName, email, password })
+            .post(`${apiHost}/api/auth/create-account`)
+            .send({ firstName, lastName, email, password })
 
     }
 
     requestUserObject = (token) => {
         return superagent
-        .get(`${apiHost}/api/auth/me`)
-        .set('authorisation', token)
+            .get(`${apiHost}/api/auth/me`)
+            .set('authorisation', token)
     }
 
     requestLogin = (email, password) => {
-       return superagent
-        .post(`${apiHost}/api/auth/login`)
-        .send({ email, password })
-         // sends a JSON post body
+        return superagent
+            .post(`${apiHost}/api/auth/login`)
+            .send({ email, password })
+        // sends a JSON post body
         // .set('X-API-Key', 'foobar')
         // .set('accept', 'json')
         // .end((err, res) => {
@@ -29,62 +29,65 @@ class Api {
         // });
     }
 
-    requestEntries = (token,days,searchTerm,moodLimit) => {
-        console.log("requesting entries in last ",days,"days")
+    requestEntries = (token, days, searchTerm, moodLimit) => {
+        console.log("requesting entries in last", days, "days.",
+            searchTerm ? `using searchterm ${searchTerm},` : " no searchterm,", moodLimit ? `with mood limit ${moodLimit}`:"no mood limit")
         //could do this url-encoded but right now it's in the header instead, just like the token
         //returns an array of entries belonging to the user, limited to the amount specified
         return superagent
-        .get(`${apiHost}/api/entries`)
-        .set({'authorisation': token,
-            'days':days,
-            'searchTerm':searchTerm,
-            'moodLimit':moodLimit
-        })
-         
+            .get(`${apiHost}/api/entries`)
+            .set({
+                'authorisation': token,
+                'days': days,
+                'searchTerm': searchTerm,
+                'moodLimit': moodLimit
+            })
+
     }
-    requestGeotaggedEntries = (token,days,searchTerm,moodLimit) =>{
+    requestGeotaggedEntries = (token, days, searchTerm, moodLimit) => {
         // works just like requestEntries, except
         // only includes entries which are geotagged
         return superagent
-        .get(`${apiHost}/api/geotags`)
-        .set({'authorisation': token,
-            'days':days,
-            'searchTerm':searchTerm,
-            'moodLimit':moodLimit
-        })
+            .get(`${apiHost}/api/geotags`)
+            .set({
+                'authorisation': token,
+                'days': days,
+                'searchTerm': searchTerm,
+                'moodLimit': moodLimit
+            })
     }
 
     requestSingleEntry = (id, token) => {
         return superagent
-        .get(`${apiHost}/api/entries/${id}`)
-        .set('authorisation', token)
-    //    .then(reply => console.log('reply single ', reply.body))
+            .get(`${apiHost}/api/entries/${id}`)
+            .set('authorisation', token)
+        //    .then(reply => console.log('reply single ', reply.body))
     }
 
     createSingleEntry = (entryDataObj, token) => {
         return superagent
-        .post(`${apiHost}/api/entries`)
-        .set('authorisation', token)
-        .send(entryDataObj)
+            .post(`${apiHost}/api/entries`)
+            .set('authorisation', token)
+            .send(entryDataObj)
     }
-    getUnsplashImage = (searchQuery) =>{
-        console.log("searching for images with query:",searchQuery)
+    getUnsplashImage = (searchQuery) => {
+        console.log("searching for images with query:", searchQuery)
         return superagent
-        .get(`${unsplashHost}/photos/random/?client_id=7f92d3ebf06a461e3f677efbacab2e65ea790ac68f8eac6c7d46794de6da3bcf&orientation=squarish&${searchQuery}`)
+            .get(`${unsplashHost}/photos/random/?client_id=7f92d3ebf06a461e3f677efbacab2e65ea790ac68f8eac6c7d46794de6da3bcf&orientation=squarish&${searchQuery}`)
     }
     requestLogout = (token) => {
         return superagent
-        .delete(`${apiHost}/api/auth/logout`)
-        .set('authorisation', token)
-        //cause superagent's voodoo magic needs to have a .then 
-        .then(response=>console.log(response))
+            .delete(`${apiHost}/api/auth/logout`)
+            .set('authorisation', token)
+            //cause superagent's voodoo magic needs to have a .then 
+            .then(response => console.log(response))
     }
 
     requestDeleteEntry = (id, token) => {
         return superagent
-        .delete(`${apiHost}/api/entries/${id}` )
-        .set('authorisation', token)
-        .then (reply => console.log('working', reply) )
+            .delete(`${apiHost}/api/entries/${id}`)
+            .set('authorisation', token)
+            .then(reply => console.log('working', reply))
     }
 
     requestLatLong = (address) => {
